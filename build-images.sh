1381a7dee5b3dd348d398eee2649b37f487fd24b
root <root@ip-172-31-35-148.eu-west-1.compute.internal>
2019-12-11 21:36:09 +0000

    Fixed deployment on docker

--- build-images.sh
+++ build-images.sh
@@ -6,3 +6,4 @@ docker build -t spark-base:latest ./docker/base
 docker build -t spark-master:latest ./docker/spark-master
 docker build -t spark-worker:latest ./docker/spark-worker
 docker build -t spark-submit:latest ./docker/spark-submit
+docker build -t pyspark-notebook:latest ./docker/pyspark_notebook
